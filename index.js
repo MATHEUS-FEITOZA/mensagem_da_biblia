199a6213e33cb0fdb772bad32aff181892f86563
MATHEUS-FEITOZA <matheusgimenez84@gmail.com>
2022-09-14 20:14:54 -0300

    consumindo da api

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-let menssagem = document.getElementById("menssagemPrincipal");
+
+
+
+
 let btnBuscar = document.getElementById("buscar");
 
 
 
-const buscaMenssagem = async() => {
-    
-    let url ='https://positive-vibes-api.herokuapp.com/quotes';
-    let promisse =await fetch(url);
-    // let menssagem = await promisse.json();
-    console.log(promisse);
+const buscaMenssagem = async () => {
+
+    let url = "https://www.abibliadigital.com.br/api/verses/nvi/sl/random";
+    let promisse = await fetch(url);
+    let menssagem = await promisse.json();
+    let menssagemPrincipal = document.getElementById("menssagemPrincipal").innerHTML = menssagem.text;
 }
 
 btnBuscar.addEventListener("click", buscaMenssagem);
